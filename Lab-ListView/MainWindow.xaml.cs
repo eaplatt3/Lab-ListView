177cdecb2033eb68f8b334835276fee8aacab20d
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-10-24 15:59:31 -0400

    Add project files.

---
+++ Lab-ListView/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab_ListView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string tempPrice;
+
+            VideoGame vg = new VideoGame();
+
+            vg.GameName = GameNameTextBox.Text;
+            vg.Rating = RatingTextBox.Text;
+            tempPrice = PriceTextBox.Text;
+            if (!double.TryParse(tempPrice, out double i)) { }
+            vg.Price = i;
+
+            VideoGameListView.Items.Add(vg);
+
+            GameNameTextBox.Text = " ";
+            RatingTextBox.Text = " ";
+            PriceTextBox.Text = " ";
+
+            GameNameTextBox.Focus();
+        }
+
+    }
+}
---
+++ Lab-ListView/VideoGame.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_ListView
+{
+    class VideoGame
+    {
+        private string m_GameName;
+        private string m_Rating;
+        private double m_Price;
+
+        public string GameName
+        {
+            get
+            {
+                return m_GameName;
+            }
+
+            set
+            {
+                m_GameName = value;
+
+            }
+        }
+
+        public string Rating
+        {
+            get
+            {
+                return m_Rating;
+            }
+
+            set
+            {
+                m_Rating = value;
+
+            }
+        }
+
+        public double Price
+        {
+            get
+            {
+                return m_Price;
+            }
+
+            set
+            {
+                m_Price = value;
+
+            }
+        }
+
+        public VideoGame()
+        {
+            GameName = " ";
+            Rating = " ";
+            Price = 0.0;
+        }
+
+    }
+}
